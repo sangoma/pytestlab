fb79b382f8fa09264614e6b0953d2bd60c1fb85a
Tyler Goodlet <tgoodlet@gmail.com>
2016-09-01 01:12:17 -0400

    Make all env commands Listers

--- lab/app/environments.py
+++ lab/app/environments.py
@@ -3,8 +3,15 @@ from cliff.command import Command
 from cliff.lister import Lister
 
 
+def get_items(cmd, parsed_args, entry=None):
+    entry = entry or cmd.app.get_environment(parsed_args.name)
+    items = sorted(entry.view.iteritems(), key=operator.itemgetter(0))
+    return (('role', 'hosts'),
+            ((role, ','.join(hosts)) for role, hosts in items))
+
+
 class EnvLister(Lister):
-    "show an environment"
+    "Show an environment"
 
     def get_parser(self, prog_name):
         parser = super(EnvLister, self).get_parser(prog_name)
@@ -12,14 +19,11 @@ class EnvLister(Lister):
         return parser
 
     def take_action(self, parsed_args):
-        entry = self.app.get_environment(parsed_args.name)
-        items = sorted(entry.view.iteritems(), key=operator.itemgetter(0))
-        return (('role', 'hosts'),
-                ((role, ','.join(hosts)) for role, hosts in items))
+        return get_items(self, parsed_args)
 
 
-class EnvRegister(Command):
-    "register a role to an environment"
+class EnvRegister(Lister):
+    "Register a role to an environment"
 
     def get_parser(self, prog_name):
         parser = super(EnvRegister, self).get_parser(prog_name)
@@ -31,10 +35,11 @@ class EnvRegister(Command):
     def take_action(self, parsed_args):
         entry = self.app.get_environment(parsed_args.name)
         entry.register(parsed_args.role, parsed_args.host)
+        return get_items(self, parsed_args, entry)
 
 
-class EnvUnregister(Command):
-    "unregister a role from an environment"
+class EnvUnregister(Lister):
+    "Unregister a role from an environment"
 
     def get_parser(self, prog_name):
         parser = super(EnvUnregister, self).get_parser(prog_name)
@@ -47,3 +52,4 @@ class EnvUnregister(Command):
     def take_action(self, parsed_args):
         entry = self.app.get_environment(parsed_args.name)
         entry.unregister(parsed_args.role, parsed_args.host)
+        return get_items(self, parsed_args, entry)
