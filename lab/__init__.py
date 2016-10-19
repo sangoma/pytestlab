8f9285c0b71134fc3d3377cff1a44cca7fd223b1
Tyler Goodlet <tgoodlet@gmail.com>
2016-10-19 03:04:01 -0400

    Make storage plugin a pytest-lab built-in
    QA-280 #In-Progress
    QA-281 #In-Progress

--- lab/__init__.py
+++ lab/__init__.py
@@ -1,2 +1,5 @@
 from .app.__main__ import load_backends
 from .model import Environment, Equipment
+
+# built-in plugin loading
+pytest_plugins = ('lab.storage')
---
+++ lab/storage.py
@@ -0,0 +1,127 @@
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+#
+# Copyright (C) 2015 Sangoma Technologies Corp.
+# All Rights Reserved.
+
+import pytest
+import re
+import plumbum
+import py
+import itertools
+
+
+def sanitized_name(name):
+    return re.sub('[^\[\]\.\w\s\d_-]', '_', name).strip()
+
+
+class ArtifactPath(object):
+    def __init__(self, path=None):
+        if path is None:
+            # autogenerate a tmpdir of the form '/tmp/pytest-<int>'
+            self.path = plumbum.local.path(str(
+                py.path.local.make_numbered_dir(
+                    prefix="pytest-",
+                    rootdir=py.path.local("/tmp/"),
+                    keep=5,
+                )
+            ))
+        else:
+            self.path = plumbum.local.path(path)
+
+        self.path.mkdir()
+        for entity in self.path.list():
+            self.path.join(entity).delete()
+
+    def copy(self, remote, local=None):
+        if not isinstance(remote, plumbum.Path):
+            remote = plumbum.local.path(remote)
+        target = self.path.join(local or remote.basename)
+        plumbum.path.copy(remote, target)
+        return target
+
+    def open(self, path, mode='r'):
+        target = self.path.join(path)
+        return target.open(mode)
+
+    def substorage(self, path):
+        target = self.path.join(path)
+        return ArtifactPath(target)
+
+    # plumbum compatability API
+    def join(self, path):
+        return self.path.join(path)
+
+    def __str__(self):
+        return str(self.path)
+
+    @property
+    def basename(self):
+        return self.path.basename
+
+
+class StorageManager(object):
+    def __init__(self, root):
+        self.root = ArtifactPath(root)
+        self.storage_cache = {}
+        self.counter = itertools.count(1)
+
+    def join(self, path):
+        try:
+            return self.storage_cache[path]
+        except KeyError:
+            store = self.root.substorage(path)
+            self.storage_cache[path] = store
+            return store
+
+    def get_storage(self, item):
+        storagedir = getattr(item, '_storagedir', None)
+        if not storagedir:
+            storagedir = '{:03}-{}'.format(next(self.counter),
+                                           sanitized_name(item.name))
+            item._storagedir = storagedir
+        return self.join(storagedir)
+
+    def pytest_runtest_setup(self, item):
+        # Make sure the test storage directory is created, even if the
+        # test doesn't happen to use it
+        self.get_storage(item)
+
+    @pytest.hookimpl
+    def pytest_lab_process_logs(self, config, item, logs):
+        for ctl, logset in logs.iteritems():
+            prefix_path = '@'.join((ctl.name, ctl.hostname))
+            prefix_dir = self.get_storage(item).join('logs').join(prefix_path)
+            prefix_dir.mkdir()
+
+            for remotefile, contents in logset.iteritems():
+                filename = str(remotefile)
+                localfile = prefix_dir.join(
+                    filename.lstrip(remotefile.root).replace('/', '_')
+                )
+                localfile.write(contents)
+                pytest.log.info('Archived {}'.format(filename))
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('storage')
+    group.addoption('--storage-root', action='store', dest='storage',
+                    default=None, metavar='DIRECTORY',
+                    help='set the target directory where test storage '
+                         'should be stored')
+
+
+def pytest_cmdline_main(config):
+    storage = StorageManager(config.option.storage)
+    config.pluginmanager.register(storage, "storage")
+
+
+@pytest.fixture
+def storage(request):
+    store = request.config.pluginmanager.getplugin("storage")
+    return store.get_storage(request.node)
+
+
+@pytest.fixture(scope='session')
+def pkgcache(request):
+    store = request.config.pluginmanager.getplugin("storage")
+    return store.join('pkgcache')
