6f6e373cc629613f686b81d581c5233c3d55348c
Tyler Goodlet <tgoodlet@gmail.com>
2016-10-24 12:08:37 -0400

    Improve unit tests
    Improve mocking to verify role factory calling and simulate unique
    `base.Controller` derived instances. Verify that roles are unregistered
    from pluggy after destruction.
    
    QA-269 #In-Progress

--- unittests/test_roles.py
+++ unittests/test_roles.py
@@ -6,10 +6,25 @@ def mockctl(testdir):
     testdir.makepyfile(mockctl="""
         import pytest
         import mock
+        from sangoma.controllers import base
+
         pytest_plugins = 'sangoma.lab.roles'
 
+        class MyCtl(base.Controller):
+            def __init__(self, location, **kwargs):
+                super(type(self), self).__init__(location, **kwargs)
+                self.setup = mock.MagicMock()
+                self.teardown = mock.MagicMock()
+
+        # mocks a role factory definition
+        global_mock = mock.Mock(side_effect=MyCtl)
+
+        @pytest.fixture
+        def mock_factory():
+            return global_mock
+
         def pytest_lab_addroles(rolemanager):
-            rolemanager.register('mock', mock.Mock())
+            rolemanager.register('mock', global_mock)
 
         @pytest.fixture
         def localhost():
@@ -38,7 +53,7 @@ def test_manage_location(testdir):
     assert result.ret == 0
 
 
-def test_register_role(mockctl, testdir):
+def test_role_loading(mockctl, testdir):
     testdir.makeconftest("""
         pytest_plugins = 'mockctl'
 
@@ -52,9 +67,14 @@ def test_register_role(mockctl, testdir):
     """)
 
     testdir.makepyfile("""
-        def test_add_role(localhost):
-            role = localhost.role('mock')
+        def test_add_role(localhost, mock_factory):
+            role = localhost.role('mock', doggy='doggy')
+
+            # test location arg and kwargs pass through on build
+            mock_factory.assert_called_once_with(localhost, doggy='doggy')
+
             assert role.name == 'mock'
+            assert role.location == localhost
             role.setup.assert_called_once_with()
 
             localhost.destroy(role)
@@ -65,7 +85,7 @@ def test_register_role(mockctl, testdir):
     assert result.ret == 0
 
 
-def test_register_unknown_role(mockctl, testdir):
+def test_load_unknown_role(mockctl, testdir):
     testdir.makeconftest("""
         pytest_plugins = 'mockctl'
     """)
@@ -107,15 +127,20 @@ def test_register_after_deletion(mockctl, testdir):
     """)
 
     testdir.makepyfile("""
+        import pytest
+
         def test_add_role(localhost):
             mock1 = localhost.role('mock')
             assert mock1.name == 'mock'
 
             localhost.destroy(mock1)
+            assert mock1 not in localhost.roles
+            # role deletion should also do plugin deregistration
+            assert not pytest.env.config.pluginmanager.is_registered(mock1)
 
             mock2 = localhost.role('mock')
             assert mock2.name == 'mock'
-            assert mock1 != mock2
+            assert mock1 is not mock2
     """)
 
     result = testdir.runpytest_subprocess('--env', 'mock')
