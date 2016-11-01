5da9b4d6bc2a6a82add2cf22aeef82416ed4e35d
Tyler Goodlet <tgoodlet@gmail.com>
2016-11-01 19:32:21 -0400

    Add a custom connection test
    Verify the new API for registering a custom connection works
    using a mock driver. Ensures that,
    - factory is called
    - connections are lazy loaded via the `connection` descriptor
    - attribute and magic method proxying works

--- unittests/test_connections.py
+++ unittests/test_connections.py
@@ -1,5 +1,6 @@
 import pytest
-from lab.comms import connection
+import mock
+from lab import comms
 from lab.ssh import SSHCommsError, ProcessExecutionError
 
 pytest_plugins = ('lab')
@@ -10,7 +11,7 @@ class MyCtl(object):
         self.config = config
         self.location = location
 
-    ssh = connection('ssh')
+    ssh = comms.connection('ssh')
 
 
 class Plugin:
@@ -21,12 +22,58 @@ class Plugin:
         rolemanager.register('myctl', MyCtl)
 
 
-@pytest.fixture
-def myctl(pytestconfig):
+@pytest.fixture(scope='session')
+def localhost(pytestconfig):
     pytestconfig.pluginmanager.register(Plugin)
     return pytest.env.manage(
-        'localhost', facts={'port': 2222, 'password': 'root'}
-    ).role('myctl')
+        'localhost', facts={'port': 2222, 'password': 'root'})
+
+
+@pytest.fixture
+def myctl(localhost):
+    return localhost.role('myctl')
+
+
+@pytest.fixture
+def custom_conn():
+    """Add a custom connection to a ctl to verify the api works
+    as expected.
+    """
+    kwargs = {'user': 'root', 'password': 'doggy'}
+    inst = mock.MagicMock()
+    factory = mock.MagicMock(return_value=inst)
+    comms.register(
+        name='mock',
+        factory=factory,
+        # always is up
+        is_up=lambda driver: driver,
+        magic_methods=['__getitem__'],
+        **kwargs
+    )
+    conn = comms.connection('mock')
+    MyCtl.mock = conn
+    yield conn, factory, inst, kwargs
+    del MyCtl.mock
+
+
+def test_custom_proxied_connection(custom_conn, localhost):
+    conn, factory, inst, kwargs = custom_conn
+
+    assert conn.key in comms._registry
+
+    # verify factory calling and lazy loading
+    assert conn._proxy is None
+    myctl = localhost.role('myctl')  # pytest getattrs the world...
+    factory.assert_called_once_with(localhost, **kwargs)
+    assert conn._proxy.driver is inst
+
+    # verify attr proxying
+    myctl.mock.test()
+    inst.test.assert_called_once_with()
+
+    # verify magic method proxying
+    myctl.mock['key']
+    inst.__getitem__.called_once_with('key')
 
 
 def test_reliable_ssh(myctl, alpine_ssh):
