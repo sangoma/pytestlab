2908bfb0ec0c38065411f1f43d7353cf0d70cc87
Tyler Goodlet <tgoodlet@gmail.com>
2017-02-26 15:45:23 -0500

    Add license headers
    We decided to stick with our favorite; MPL 2.0!
    
    QA-463 #In-Progress

--- lab/network/forward.py
+++ lab/network/forward.py
@@ -1,6 +1,10 @@
 #
 # Copyright 2017 Sangoma Technologies Inc.
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
 # Adapted for our own use from
 # https://github.com/paramiko/paramiko/blob/master/demos/forward.py
 from future import standard_library
