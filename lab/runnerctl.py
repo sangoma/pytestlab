f8936c7ff14857ca38504d025424469d53fa13e3
Simon Gomizelj <simon@vodik.xyz>
2017-03-06 13:14:37 -0500

    Organize runnerctl plugin

--- lab/runnerctl.py
+++ lab/runnerctl.py
@@ -8,14 +8,10 @@
 pytest runner control plugin
 """
 from builtins import object
-import pytest
 import string
+import pytest
 
 
-def pytest_runtest_makereport(item, call):
-    if 'setup_test' in item.keywords and call.excinfo:
-        if not call.excinfo.errisinstance(pytest.skip.Exception):
-            pytest.halt('A setup test has failed, aborting...')
 
 
 class Halt(object):
@@ -37,6 +33,12 @@ def pytest_runtest_protocol(item, nextitem):
     yield
     if pytest.halt.msg:
         item.session.shouldstop = pytest.halt.msg
+
+
+def pytest_runtest_makereport(item, call):
+    if 'setup_test' in item.keywords and call.excinfo:
+        if not call.excinfo.errisinstance(pytest.skip.Exception):
+            pytest.halt('A setup test has failed, aborting...')
 
 
 @pytest.fixture(scope='class')
